95088ee324e604fbf09533d883eacd19106bf202
huzaifa8883 <aw3540341@gmail.com>
2025-03-13 05:52:36 +0500

    Dfdfdf

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,16 +1,23 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import { dbname } from "../constant.js";
 
-const connectdb = async()=>{
-    try {
-        const connectstring = await mongoose.connect(`${process.env.MONGODBBURL}`)
-        console.log(connectstring.connection.host)
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
-    
-}
+dotenv.config(); // Load environment variables
 
-export default connectdb
+const connectdb = async () => {
+    try {
+        const connectionString = `mongodb+srv://website:${process.env.DB_PASSWORD}@cluster0.neu99.mongodb.net/${dbname}?retryWrites=true&w=majority&appName=Cluster0`;
+        
+        const connection = await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log(`MongoDB Connected: ${connection.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB Connection Error:", error);
+        process.exit(1);
+    }
+};
+
+export default connectdb;
